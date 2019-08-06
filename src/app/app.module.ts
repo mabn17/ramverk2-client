f993041df67965e0749b4390882915251904ee58
Martin <martin.d@live.se>
2019-08-06 20:08:58 +0200

    generated components for kmom01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
+import { ReportDetailsComponent } from './report-details/report-details.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ReportsComponent,
+    ReportDetailsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
