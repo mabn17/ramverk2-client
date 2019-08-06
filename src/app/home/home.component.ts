f02e2c857a5679aadf142d9edb15770834ad0dae
Martin <martin.d@live.se>
2019-08-06 20:32:13 +0200

    created base for home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private data: any;
+  private errorMessage = 'Placeholder';
+
   constructor() { }
 
   ngOnInit() {
+    // Figure out how to get data from api
   }
 
 }
